4e905ff5b597cf17b22b3b4765a905b4f62734e1
Mario Neises <git@mnio.net>
2015-11-26 20:40:17 +0100

    add input key listener

--- src/UserInput.js
+++ src/UserInput.js
@@ -6,16 +6,20 @@ export default class UserInput extends React.Component {
   constructor(props) {
     super(props)
 
-    _.bindAll(this, 'showPassword')
+    _.bindAll(this, 'showPassword', 'updateHashs')
   }
 
   showPassword() {
 
   }
 
+  updateHashs(event) {
+    console.log(event.target.value)
+  }
+
   render() {
     return (<div>
-      Identifier: <input type="text" /> Token: <input type="password" />
+      Identifier: <input type="text" onKeyUp={this.updateHashs} /> Token: <input type="password" onKeyUp={this.updateHashs} />
       <span className="glyphicon glyphicon-eye-open pointer" aria-hidden="true" onClick={this.showPassword}></span>
       <span></span>
     </div>)
