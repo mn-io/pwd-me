b8801043afd1141af31b03795c09a2a21c060343
Mario Neises <git@mnio.net>
2015-11-26 21:06:06 +0100

    add ids for input

--- src/UserInput.js
+++ src/UserInput.js
@@ -6,6 +6,11 @@ export default class UserInput extends React.Component {
   constructor(props) {
     super(props)
 
+    this.identifierFieldName = "identifierField"
+    this.tokenFieldName = "tokenField"
+
+    this.inputs = {}
+
     _.bindAll(this, 'showPassword', 'updateHashs')
   }
 
@@ -14,12 +19,20 @@ export default class UserInput extends React.Component {
   }
 
   updateHashs(event) {
-    console.log(event.target.value)
+    // console.log(event.target)
+
+    this.inputs[event.target.id] = event.target.value
+    if(!this.inputs[this.identifierFieldName] || !this.inputs[this.tokenFieldName]) {
+      return
+    }
+
+    console.log("yea")
   }
 
   render() {
     return (<div>
-      Identifier: <input type="text" onKeyUp={this.updateHashs} /> Token: <input type="password" onKeyUp={this.updateHashs} />
+      Identifier: <input type="text" onKeyUp={this.updateHashs} id={this.identifierFieldName} />
+      Token: <input type="password" onKeyUp={this.updateHashs} id={this.tokenFieldName} />
       <span className="glyphicon glyphicon-eye-open pointer" aria-hidden="true" onClick={this.showPassword}></span>
       <span></span>
     </div>)
