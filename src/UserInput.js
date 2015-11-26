2b756450b9cc2327acb63372d07a755b265b042b
Mario Neises <git@mnio.net>
2015-11-26 19:27:54 +0100

    add basic user input

--- src/UserInput.js
+++ src/UserInput.js
@@ -1,7 +1,23 @@
 import React from 'react'
+import _ from 'lodash'
 
 export default class UserInput extends React.Component {
+
+  constructor(props) {
+    super(props)
+
+    _.bindAll(this, 'showPassword')
+  }
+
+  showPassword() {
+    
+  }
+
   render() {
-    return (<div>Hello World</div>)
+    return (<div>
+      Identifier: <input type="text" /> Token: <input type="password" />
+      <span className="glyphicon glyphicon-eye-open pointer" aria-hidden="true" onClick={this.showPassword}></span>
+      <span></span>
+    </div>)
   }
 }
