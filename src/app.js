073ab90fa688ceb449e6eac2ed74ac3e2053295d
Mario Neises <git@mnio.net>
2015-11-26 19:40:56 +0100

    add hash output component

--- src/app.js
+++ src/app.js
@@ -3,8 +3,9 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import UserInput from './UserInput'
 import TimeEpocheHelper from './TimeEpocheHelper'
+import HashOutput from './HashOutput'
 
-window.onload = function() {
+window.onload = () => {
   ReactDOM.render(<UserInput />, document.getElementById('userInput'))
   ReactDOM.render(<TimeEpocheHelper intervalInMin={3} />, document.getElementById('helperField'))
 }
