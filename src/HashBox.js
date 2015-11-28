9c5a60aa178b21f820775c1f5f6d68cdd8488150
Mario Neises <git@mnio.net>
2015-11-28 18:21:44 +0100

    translation for hashs

--- src/HashBox.js
+++ src/HashBox.js
@@ -8,17 +8,17 @@ export default class HashBox {
 
     this.config = {
       rows: config.outputRows,
-      salt: config.salt,
-      iterations: config.pbkdf2Iterations
+      tokenSalt: config.tokenSalt,
+      keySalt: config.keySalt,
+      iterations: config.pbkdf2Iterations,
+      chars: config.validCharacters
     }
 
     this.state = {
-      epocheCount: 0,
-      identifier: "",
-      tokenHash: ""
+      epocheCount: 0
     }
 
-    _.bindAll(this, 'identifierChanged', 'tokenChanged', 'timeEpocheChanged', 'createHashs')
+    _.bindAll(this, 'identifierChanged', 'tokenChanged', 'timeEpocheChanged', 'createHashs', 'translateHash')
 
     radio.subscribe('identifierChanged', this.identifierChanged)
     radio.subscribe('tokenChanged', this.tokenChanged)
@@ -26,12 +26,24 @@ export default class HashBox {
   }
 
   identifierChanged(identifier) {
+    if("" == identifier) {
+      this.state.identifier = null
+      //TODO: clear hashs
+      return
+    }
+
     this.state.identifier = identifier
     this.createHashs()
   }
 
   tokenChanged(token) {
-    let hash = pbkdf2(token, this.config.salt , this.config.iterations , 64) //TOOD: extract config
+    if("" == token) {
+      this.state.token = null
+      //TODO: clear hashs
+      return
+    }
+
+    let hash = pbkdf2(token, this.config.tokenSalt , this.config.iterations , 64) //TOOD: extract config
     // console.log(hash.toString('hex'))
     this.state.tokenHash = hash
     this.createHashs()
@@ -43,15 +55,31 @@ export default class HashBox {
   }
 
   createHashs() {
+    if(!this.state.identifier || !this.state.tokenHash) {
+      return
+    }
+
     let key = this.state.identifier +  this.state.tokenHash + this.state.epocheCount
 
     let hashs = []
     for (let i = 0; i < this.config.rows; i++) {
-      let currentHash = pbkdf2(key, i + "sall2", 1, 64) //TOOD: extract config
-      //TODO: continue here with translation table
-      hashs.push([currentHash, currentHash])
+      let currentHash = pbkdf2(key, i + this.config.keySalt, 1, 64) //TOOD: extract config
+      let readablePwd = this.translateHash(currentHash)
+      hashs.push([currentHash, readablePwd])
     }
 
     this.radio.broadcast('hashsCalculated', hashs)
+  }
+
+  translateHash(hash) {
+    let result = []
+    let position = 0
+
+    for (let i = 0; i < hash.length; i=i+1) {
+      position = (position + hash[i]) % this.config.chars.length
+      result.push(this.config.chars[position])
+    }
+
+    return result.join("")
   }
 }
