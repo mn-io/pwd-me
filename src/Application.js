d55bf2c612501abf6f4c729d7e15abad001bef40
Mario Neises <git@mnio.net>
2015-12-22 16:58:42 +0100

    moved/cleanup config.json + loding async config with fallback

--- src/Application.js
+++ src/Application.js
@@ -1,23 +1,37 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import Airwaves from 'airwaves'
+import xhr from 'xhr'
 
 import UserInput from './UserInput'
 import HashOutput from './HashOutput'
 import HashBox from './HashBox'
 
-import config from '../public/config/config.json'
+import builtinConfig from '../public/config.json'
 
+let configUrl = 'config.json'
 let radio = new Airwaves.Channel()
-let callback = (hashs) => {radio.broadcast('hashsCalculated', hashs)}
-let box = new HashBox(config, callback)
 
-radio.subscribe('setIdentifier', box.setIdentifier)
-radio.subscribe('setToken', box.setToken)
-radio.subscribe('setTimeEpoche', box.setTimeEpoche)
+let xhrCallback = (err, resp) => {
+  let config = builtinConfig
+  if(!err) {
+    console.log('Loading configuration from "' + configUrl + '": OK')
+    config = resp.body
+  } else {
+    console.log('Loading configuration from "' + configUrl + '": FAILED, using builtin fallback, error was: "' +  err + '"')
+  }
 
+  let callback = (hashs) => {radio.broadcast('hashsCalculated', hashs)}
+  let box = new HashBox(config, callback)
+
+  radio.subscribe('setIdentifier', box.setIdentifier)
+  radio.subscribe('setToken', box.setToken)
+  radio.subscribe('setTimeEpoche', box.setTimeEpoche)
+
+  ReactDOM.render(<HashOutput config={config} radio={radio} />, document.getElementById('hashOutput'))
+}
 
 window.onload = () => {
   ReactDOM.render(<UserInput radio={radio} />, document.getElementById('userInput'))
-  ReactDOM.render(<HashOutput config={config} radio={radio} />, document.getElementById('hashOutput'))
+  xhr.get(configUrl, xhrCallback)
 }
