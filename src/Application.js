67a17fbe022920663ab41cef8091648e58990f43
Mario Neises <git@mnio.net>
2016-03-30 17:52:45 +0200

    reorder funcs

--- src/Application.js
+++ src/Application.js
@@ -15,38 +15,36 @@ let radio = new Airwaves.Channel()
 
 Logger.interceptConsole(radio)
 
-window.onload = () => {
-  let loadConfig = (err, resp, defaultConfig) => {
-    if(err || !resp.body) {
-      console.warn('Loading configuration from "' + configUrl + '": FAILED, using builtin fallback, error occured on loading: "' +  err + '"')
-      return defaultConfig
-    }
+window.onload = () => xhr.get(configUrl, xhrCallback)
 
-    try {
-      let config = JSON.parse(resp.body)
-      console.log('Loading configuration from "' + configUrl + '": OK')
-      return config
-    } catch (e) {
-      console.warn('Loading configuration from "' + configUrl + '": FAILED, using builtin fallback, error occured on parsing: "' +  e + '"')
-      return defaultConfig
-    }
+let xhrCallback = (err, resp) => {
+  let config = loadConfig(err, resp, builtinConfig)
+
+  let callback = (hashs) => {radio.broadcast('hashsCalculated', hashs)}
+  let box = new HashBox(config.hashBox, callback)
+  box.setProfilesConfig(config.profiles)
+
+  radio.subscribe('setIdentifier', box.setIdentifier)
+  radio.subscribe('setToken', box.setToken)
+  radio.subscribe('setTimeEpoche', box.setTimeEpoche)
+  radio.subscribe('setProfileByName', box.setProfileByName)
+
+  ReactDOM.render(<UserInput profiles={config.profiles} config={config.hashBox} radio={radio} />, document.getElementById('userInput'))
+  ReactDOM.render(<HashOutput config={config.UI} radio={radio} />, document.getElementById('hashOutput'))
+}
+
+let loadConfig = (err, resp, defaultConfig) => {
+  if(err || !resp.body) {
+    console.warn('Loading configuration from "' + configUrl + '": FAILED, using builtin fallback, error occured on loading: "' +  err + '"')
+    return defaultConfig
   }
 
-  let xhrCallback = (err, resp) => {
-    let config = loadConfig(err, resp, builtinConfig)
-
-    let callback = (hashs) => {radio.broadcast('hashsCalculated', hashs)}
-    let box = new HashBox(config.hashBox, callback)
-    box.setProfilesConfig(config.profiles)
-
-    radio.subscribe('setIdentifier', box.setIdentifier)
-    radio.subscribe('setToken', box.setToken)
-    radio.subscribe('setTimeEpoche', box.setTimeEpoche)
-    radio.subscribe('setProfileByName', box.setProfileByName)
-
-    ReactDOM.render(<UserInput profiles={config.profiles} config={config.hashBox} radio={radio} />, document.getElementById('userInput'))
-    ReactDOM.render(<HashOutput config={config.UI} radio={radio} />, document.getElementById('hashOutput'))
+  try {
+    let config = JSON.parse(resp.body)
+    console.log('Loading configuration from "' + configUrl + '": OK')
+    return config
+  } catch (e) {
+    console.warn('Loading configuration from "' + configUrl + '": FAILED, using builtin fallback, error occured on parsing: "' +  e + '"')
+    return defaultConfig
   }
-
-  xhr.get(configUrl, xhrCallback)
 }
