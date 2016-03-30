b373a059e4ff332d3de067794802f3e22a6ac5f1
Mario Neises <git@mnio.net>
2016-03-30 17:31:59 +0200

    reformat + removed instant calculation

--- src/UserInput.react.js
+++ src/UserInput.react.js
@@ -52,14 +52,6 @@ export default class UserInput extends ReactComponent {
     if(this.timerToken) {
       clearTimeout(this.timerToken)
     }
-
-    if(!this.state.isInstantGeneration) {
-      return
-    }
-
-    this.timerToken = setTimeout(() => {
-      this.props.radio.broadcast('setToken', token)
-    }, 400)
   }
 
   setIdentifier(event) {
@@ -72,14 +64,6 @@ export default class UserInput extends ReactComponent {
     if(this.timerIdentifier) {
       clearTimeout(this.timerIdentifier)
     }
-
-    if(!this.state.isInstantGeneration) {
-      return
-    }
-
-    this.timerIdentifier = setTimeout(() => {
-      this.props.radio.broadcast('setIdentifier', identifier)
-    }, 200)
   }
 
   selectProfile(event) {
@@ -89,24 +73,6 @@ export default class UserInput extends ReactComponent {
     })
 
     this.clearAutoDestroy()
-    if(!this.state.isInstantGeneration) {
-      return
-    }
-
-    this.props.radio.broadcast('setProfileByName', profile)
-  }
-
-  toggleInstantGeneration() {
-    this.clearAutoDestroy()
-
-    let isEnabled = !this.state.isInstantGeneration
-    this.setState({
-      isInstantGeneration: isEnabled
-    })
-
-    if(isEnabled) {
-      this.sendData()
-    }
   }
 
   toggleAutoDestroy() {
@@ -138,15 +104,11 @@ export default class UserInput extends ReactComponent {
       event.preventDefault()
     }
 
-    if(this.state.isInstantGeneration) {
-      return
-    }
-
-    this.setState({})
-
     this.props.radio.broadcast('setProfileByName', this.state.inputProfile)
     this.props.radio.broadcast('setIdentifier', this.state.inputIdentifier)
     this.props.radio.broadcast('setToken', this.state.inputToken)
+
+    this.setState({})
   }
 
   outputGenerated(hashs) {
@@ -178,43 +140,56 @@ export default class UserInput extends ReactComponent {
         <form className='form-horizontal' onSubmit={this.sendData}>
 
             <div className='input-group with-spacer'>
-              <input type={this.state.tokenFieldType} className='form-control'
-                aria-label='Token (slow calculation)' onKeyUp={this.setToken}
-                tabIndex='1' autoFocus={true} />
+              <input type={this.state.tokenFieldType}
+                className='form-control'
+                aria-label='Token (slow calculation)'
+                onKeyUp={this.setToken}
+                tabIndex='1'
+                autoFocus={true} />
               <div className='input-group-btn'>
-                <button type='button' className='btn btn-default' aria-label='Hint' onClick={this.showToken}>
+                <button type='button'
+                  className='btn btn-default'
+                  aria-label='Hint'
+                  onClick={this.showToken}>
                   <span className='glyphicon glyphicon-eye-open'></span>
                 </button>
               </div>
             </div>
 
-            <input type='text' className='form-control with-spacer' placeholder='Identifier'
-              onKeyUp={this.setIdentifier} tabIndex='2' />
+            <input type='text'
+              className='form-control with-spacer'
+              placeholder='Identifier'
+              onKeyUp={this.setIdentifier}
+              tabIndex='2' />
 
-            <select className='form-control with-spacer' onChange={this.selectProfile} tabIndex='3'>
+            <select className='form-control with-spacer'
+              onChange={this.selectProfile}
+              tabIndex='3'>
               {renderProfiles()}
             </select>
 
-          <ConfigToggle config={this.props.config} profiles={this.props.profiles} radio={this.props.radio} />
+          <ConfigToggle config={this.props.config}
+            profiles={this.props.profiles}
+            radio={this.props.radio} />
+
           <LogOutputToggle radio={this.props.radio} />
 
           <div className='checkbox'>
             <label>
-              <input type='checkbox' checked={this.state.isInstantGeneration} onChange={this.toggleInstantGeneration} />instant generate
-            </label>
-          </div>
-
-          <div className='checkbox'>
-            <label>
-              <input type='checkbox' checked={this.state.isAutoDestroy} onChange={this.toggleAutoDestroy} />reload page after 45 sec
+              <input type='checkbox'
+                checked={this.state.isAutoDestroy}
+                onChange={this.toggleAutoDestroy} />
+            reload page after 45 sec
             </label>
           </div>
 
           <TimeEpocheHelper intervalInMin={3} radio={this.props.radio} />
 
           <div className='pull-right'>
-            <button type='submit' className='btn btn-link btn-minimal' onClick={this.sendData}
-              disabled={this.state.isInstantGeneration} tabIndex='4'>generate</button>
+            <button type='submit'
+              className='btn btn-link btn-minimal'
+              onClick={this.sendData}
+              tabIndex='4'>generate</button>
           </div>
         </form>
       </div>)
