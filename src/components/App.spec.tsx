8a1c11ca7f6943ebc720f9fb76964d35782ccd94
Mario Neises <git@mnio.net>
2019-08-03 18:32:40 +0200

    basic enzyme test

--- src/components/App.spec.tsx
+++ src/components/App.spec.tsx
@@ -1,18 +1,42 @@
+import { mount } from 'enzyme'
 import * as React from 'react'
 import renderer from 'react-test-renderer'
+import Logger from '../services/logger/Logger'
 import App from './App'
 
 jest.mock('../services/configProvider/ConfigProvider')
-jest.mock('../services/logger/Logger')
+// jest.mock('../services/logger/Logger')
 
 describe('App', () => {
 
-  it('renders matching snapshot', async () => {
+  xit('renders matching snapshot', async () => {
     const tree = renderer
       .create(<App />)
 
     expect(tree.toJSON()).toMatchSnapshot()
 
     tree.unmount()
+  })
+
+  it('does a full circle', async () => {
+    const expectedLogs = [
+      {
+        channel: 'log',
+        messages: ['Mocking config loading from config.json']
+      },
+    ]
+
+
+    const wrapper = mount(<App />)
+
+    await new Promise(resolve => setTimeout(() => {
+      wrapper.update()
+
+      expect(Logger.getLogs()).toEqual(expectedLogs)
+
+      // console.log('', wrapper.html())
+      resolve()
+    }))
+
   })
 })
